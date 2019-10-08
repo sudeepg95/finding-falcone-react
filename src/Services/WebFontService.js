391186a89202013acdf4232e3016639d36396d21
Sudeep G <sudeepg95@gmail.com>
2019-10-09 00:56:43 +0530

    Fixed issue with Webfontloader

--- src/Services/WebFontService.js
+++ src/Services/WebFontService.js
@@ -1,11 +1,16 @@
-import WebFont from 'webfontloader';
+import { load as webFontLoader } from 'webfontloader';
 
 const configureWebFontService = () => {
-  WebFont.load({
-    google: {
-      families: ['Montserrat:400,500,700,700i,800']
-    }
-  });
+  if (typeof webFontLoader !== 'undefined') {
+    webFontLoader({
+      google: {
+        families: ['Montserrat:400,500,700,700i,800']
+      }
+    });
+  } else {
+    // eslint-disable-next-line no-console
+    console.log('WebFont not defined!');
+  }
 };
 
 export default configureWebFontService;
